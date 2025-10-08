6abef14302224452161ca47494ad19a6f491f46f
yanosea <81510859+yanosea@users.noreply.github.com>
2025-10-08 17:41:13 +0900

    💄style(ui): add left indent to mobile menu items
    - add `pl-4` class to mobile menu container for visual hierarchy
    - menu items now appear indented from the site title

--- src/_components/MobileMenu.tsx
+++ src/_components/MobileMenu.tsx
@@ -5,7 +5,7 @@ export default function MobileMenu() {
       className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-300 ease-in-out md:hidden"
     >
       <div className="min-h-0 overflow-hidden">
-        <div className="space-y-2 pt-6">
+        <div className="space-y-2 pt-6 pl-4">
           <a href="/about.html" className="block nav-link">about</a>
           <a href="/blog.html" className="block nav-link">blog</a>
           <a href="/links.html" className="block nav-link">links</a>
